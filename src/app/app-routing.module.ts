c37c712dfe5688dfa8e0a2bd88f4de548303ce84
Felipe Andrews <Andrews@lti08.cesed.local>
2019-02-12 21:46:21 -0300

    tela de login, cadastro e perfil quase prontas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:'', redirectTo:"profile", pathMatch:"full"},
-  {path:'', component: ProfileComponent,}
+  {path:'profile', component: ProfileComponent,},
+  {path:'cadastro', component: CadastroComponent},
+  {path:'login', component: LoginComponent},
+  {path:'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+
 
 
 @NgModule({
@@ -15,7 +17,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     ProfileComponent,
     NavbarComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
